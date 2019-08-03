654f0fc2d3b7b417dcd29a29460e3e065afd91ba
Natalie Ng <thuycnn91@gmail.com>
2019-08-02 18:18:46 -0600

    currency update

--- microblog.py
+++ microblog.py
@@ -1,7 +1,8 @@
 
 import datetime
 import os
- 
+
+import requests
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 
@@ -28,7 +29,7 @@ votes = {"yes": 0, "no": 0, "maybe": 0}
 
 @app.route("/")
 def index():
-    return render_template("index.html", votes=votes)
+    return render_template("currency.html")
 
 
 @socketio.on("submit vote")
@@ -46,3 +47,19 @@ def hello():
 @app.route('/welcome')
 def counter():
     return render_template('counter.html')
+
+@app.route('/convert', methods = ['POST'])
+def convert():
+    # Query for currency exchange rate:
+    currency = request.form.get('currency')
+    res = requests.get('http://api.fixer.io/latest', params= {
+        "base":"USD", "symbol":currency})
+    # Check if the request is successful:
+    if res.status_code != 200:
+        return jsonify({'success':False})
+     # Make sure currency is in response
+
+    data = res.json()
+    if currency not in data['rates']:
+        return jsonify({'success':False})
+    
